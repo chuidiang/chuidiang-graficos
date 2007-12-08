b77638058de534e09f172a4f6743c41001227636
chuidiang <chuidiang@6584fa83-bb45-0410-9b0b-e5b4a0a7a68e>
2007-12-08 16:03:24 +0000

    comentarios

--- GRAFICOS/PRINCIPAL/src/main/java/com/chuidiang/matematicas/funciones/Polinomio.java
+++ GRAFICOS/PRINCIPAL/src/main/java/com/chuidiang/matematicas/funciones/Polinomio.java
@@ -1,17 +1,39 @@
+/**
+ * Funcion que representa un polinomio.
+ */
 package com.chuidiang.matematicas.funciones;
 
-public class Polinomio implements IfzFuncionMatematica
-{
-	double [] coeficientes=null;
-    
-    public Polinomio (double [] coeficientes)
-    {
-        this.coeficientes=coeficientes;
+/**
+ * Un polinomio.
+ * @author chuidiang
+ *
+ */
+public class Polinomio implements IfzFuncionMatematica {
+    /**
+     * Coeficientes del polinomio, de forma que
+     * y = sumatorio coeficientes[i] * x ^ y
+     * es decir, el indice cero corresponde al termino independiente.
+     */
+    double[] coeficientes = null;
+
+    /**
+     * Construye un polinomio con los coeficientes que se le pasan
+     * @param coeficientes
+     */
+    public Polinomio(double[] coeficientes) {
+        this.coeficientes = coeficientes;
     }
 
-	public double funcion(double x)
-	{
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    /**
+     * Devuelve el valor de y para la x que se le pasa, segun la
+     * formula
+     * y = sumatorio coeficientes[i] * x ^ y
+     */
+    public double funcion(double x) {
+        double y = 0.0;
+        for (int i = 0; i < coeficientes.length; i++) {
+            y = y + (coeficientes[i] * Math.pow(x, i));
+        }
+        return y;
+    }
 }
--- GRAFICOS/PRINCIPAL/src/main/java/com/chuidiang/matematicas/funciones/Seno.java
+++ GRAFICOS/PRINCIPAL/src/main/java/com/chuidiang/matematicas/funciones/Seno.java
@@ -1,11 +1,62 @@
+/*
+ * 
+ */
 package com.chuidiang.matematicas.funciones;
 
+/**
+ * Funcion sin(x).
+ * Permite modificar amplitud y frecuencia, de forma que
+ * y = amplitud*sin(frecuencia*x)
+ * @author chuidiang
+ *
+ */
+public class Seno implements IfzFuncionMatematica {
+    /**
+     * Amplitud
+     */
+    double amplitud = 1.0;
+    /**
+     * Frecuencia
+     */
+    double frecuencia = 1.0;
 
-public class Seno implements IfzFuncionMatematica
-{
-	public double funcion(double x)
-	{
-		return Math.sin(x);
-	}
+    /**
+     * Devuelve la amplitud
+     * @return
+     */
+    public double getAmplitud() {
+        return amplitud;
+    }
+
+    /**
+     * Fija la amplitud
+     * @param amplitud
+     */
+    public void setAmplitud(double amplitud) {
+        this.amplitud = amplitud;
+    }
+
+    /**
+     * Devuelve la frecuencia
+     * @return
+     */
+    public double getFrecuencia() {
+        return frecuencia;
+    }
+
+    /**
+     * Fija la frecuencia
+     * @param frecuencia
+     */
+    public void setFrecuencia(double frecuencia) {
+        this.frecuencia = frecuencia;
+    }
+
+    /**
+     * Devuelve amplitud*sin(frecuencia*x) con la x en radianes.
+     */
+    public double funcion(double x) {
+        return amplitud * Math.sin(frecuencia * x);
+    }
 
 }
